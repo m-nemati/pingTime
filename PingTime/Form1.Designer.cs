ee38d0c0fa2de87a7ec861e11c57f900d9cbe382
m-nemati <m.nemati2002@gmail.com>
2020-06-05 00:10:51 +0430

    first release

---
+++ PingTime/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PingTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPing8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPing1 = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPing8
+            // 
+            this.btnPing8.BackColor = System.Drawing.Color.MistyRose;
+            this.btnPing8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPing8.ForeColor = System.Drawing.Color.Red;
+            this.btnPing8.Location = new System.Drawing.Point(44, 30);
+            this.btnPing8.Name = "btnPing8";
+            this.btnPing8.Size = new System.Drawing.Size(137, 55);
+            this.btnPing8.TabIndex = 0;
+            this.btnPing8.Text = "Ping 8.8.8.8";
+            this.btnPing8.UseVisualStyleBackColor = false;
+            this.btnPing8.Click += new System.EventHandler(this.btnPing8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MistyRose;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(45, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Results:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MistyRose;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(212, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 55);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ping 4.2.2.4";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnPing1
+            // 
+            this.btnPing1.BackColor = System.Drawing.Color.MistyRose;
+            this.btnPing1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPing1.ForeColor = System.Drawing.Color.Red;
+            this.btnPing1.Location = new System.Drawing.Point(380, 30);
+            this.btnPing1.Name = "btnPing1";
+            this.btnPing1.Size = new System.Drawing.Size(137, 55);
+            this.btnPing1.TabIndex = 5;
+            this.btnPing1.Text = "Ping 1.1.1.1";
+            this.btnPing1.UseVisualStyleBackColor = false;
+            this.btnPing1.Click += new System.EventHandler(this.btnPing1_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.FormattingEnabled = true;
+            this.lblResult.ItemHeight = 18;
+            this.lblResult.Location = new System.Drawing.Point(44, 131);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(473, 94);
+            this.lblResult.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.MistyRose;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(150, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Programmer:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(397, 231);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 53);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PingTime.Properties.Resources.bin;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(542, 317);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnPing1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPing8);
+            this.Name = "Form1";
+            this.Text = "Ping Time";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPing8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPing1;
+        private System.Windows.Forms.ListBox lblResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PingTime/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PingTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPing8_Click(object sender, EventArgs e)
+        {
+           try
+            {
+                Ping ping_8 = new Ping();
+                PingReply r_8;
+                string s_8;
+                s_8 = "8.8.8.8";
+                r_8 = ping_8.Send(s_8);
+                //if (r_8.Status == IPStatus.Success)
+                //{
+                //    txtResults.Text = "Ping to "  + "[" + r_8.Address.ToString() + "]" + " Successful >>"
+                //       + " Response delay = " + r_8.RoundtripTime.ToString() + " ms" + "\n";
+                //}
+
+                if (r_8.Status == IPStatus.Success)
+                {
+                    lblResult.Items.Add("Ping 8.8.8.8 >> Successful >>"
+                       + " Response delay = " + r_8.RoundtripTime.ToString() + " ms" + "\n");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("مشکلی رخ داده است. مجدد تلاش کنید.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           try
+            {
+                Ping ping_4 = new Ping();
+                PingReply r_4;
+                string s_4;
+                s_4 = "4.2.2.4";
+                r_4 = ping_4.Send(s_4);
+
+                if (r_4.Status == IPStatus.Success)
+                {
+                    lblResult.Items.Add("Ping 4.2.2.4 >> Successful >>"
+                       + " Response delay = " + r_4.RoundtripTime.ToString() + " ms" + "\n");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("مشکلی رخ داده است. مجدد تلاش کنید.");
+            }
+        }
+
+        private void btnPing1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ping ping_1 = new Ping();
+                PingReply r_1;
+                string s_1;
+                s_1 = "1.1.1.1";
+                r_1 = ping_1.Send(s_1);
+
+                if (r_1.Status == IPStatus.Success)
+                {
+                    lblResult.Items.Add("Ping 1.1.1.1 >> Successful >>"
+                       + " Response delay = " + r_1.RoundtripTime.ToString() + " ms" + "\n");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("مشکلی رخ داده است. مجدد تلاش کنید.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblResult.Items.Clear();
+        }
+    }
+}
