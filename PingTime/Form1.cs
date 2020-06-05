649229ca64b7cb48c657e49f2b32bbe863bc9d23
m-nemati <m.nemati2002@gmail.com>
2020-06-05 09:43:16 +0430

    refactor code

--- PingTime/Form1.cs
+++ PingTime/Form1.cs
@@ -18,78 +18,44 @@ namespace PingTime
 
         private void btnPing8_Click(object sender, EventArgs e)
         {
-           try
-            {
-                Ping ping_8 = new Ping();
-                PingReply r_8;
-                string s_8;
-                s_8 = "8.8.8.8";
-                r_8 = ping_8.Send(s_8);
-                //if (r_8.Status == IPStatus.Success)
-                //{
-                //    txtResults.Text = "Ping to "  + "[" + r_8.Address.ToString() + "]" + " Successful >>"
-                //       + " Response delay = " + r_8.RoundtripTime.ToString() + " ms" + "\n";
-                //}
-
-                if (r_8.Status == IPStatus.Success)
-                {
-                    lblResult.Items.Add("Ping 8.8.8.8 >> Successful >>"
-                       + " Response delay = " + r_8.RoundtripTime.ToString() + " ms" + "\n");
-                }
-            }
-            catch
-            {
-                MessageBox.Show("مشکلی رخ داده است. مجدد تلاش کنید.");
-            }
+            PingTimeRun("8.8.8.8");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-           try
-            {
-                Ping ping_4 = new Ping();
-                PingReply r_4;
-                string s_4;
-                s_4 = "4.2.2.4";
-                r_4 = ping_4.Send(s_4);
-
-                if (r_4.Status == IPStatus.Success)
-                {
-                    lblResult.Items.Add("Ping 4.2.2.4 >> Successful >>"
-                       + " Response delay = " + r_4.RoundtripTime.ToString() + " ms" + "\n");
-                }
-            }
-            catch
-            {
-                MessageBox.Show("مشکلی رخ داده است. مجدد تلاش کنید.");
-            }
+            PingTimeRun("4.2.2.4");
         }
 
         private void btnPing1_Click(object sender, EventArgs e)
         {
-            try
-            {
-                Ping ping_1 = new Ping();
-                PingReply r_1;
-                string s_1;
-                s_1 = "1.1.1.1";
-                r_1 = ping_1.Send(s_1);
-
-                if (r_1.Status == IPStatus.Success)
-                {
-                    lblResult.Items.Add("Ping 1.1.1.1 >> Successful >>"
-                       + " Response delay = " + r_1.RoundtripTime.ToString() + " ms" + "\n");
-                }
-            }
-            catch
-            {
-                MessageBox.Show("مشکلی رخ داده است. مجدد تلاش کنید.");
-            }
+            PingTimeRun("1.1.1.1");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             lblResult.Items.Clear();
+        }
+
+        public void PingTimeRun(string ipAddr)
+        {
+            try
+            {
+                Ping ping = new Ping();
+                PingReply r;
+                string s = ipAddr;
+               
+                r = ping.Send(s);
+
+                if (r.Status == IPStatus.Success)
+                {
+                    lblResult.Items.Add("Ping " + ipAddr + " >> Successful >>"
+                       + " Response delay = " + r.RoundtripTime.ToString() + " ms" + "\n");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("مشکلی رخ داده است. مجدد تلاش کنید.");
+            }
         }
     }
 }
